20f4beb1c6834f88b96c8dba53541952b7e06249
Oddisy <odunayoogunleye200@gmail.com>
2023-10-02 21:41:52 +0100

    signup page fully responsive

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -1,36 +1,39 @@
-import { logo } from "../exports";
+import {logo} from "../exports";
 import Form from "../form/signup";
 
 function Container() {
-  return (
-    <div className=" bg-bgContainer bg-cover  w-full ">
-      <div className=" bg-black bg-opacity-70 flex md:justify-center">
-        <div className=" lg:flex hidden  items-center justify-center h-screen w-1/2">
-          <div className="  h-[80vh]  w-[80%]">
-            <div>
-              <img className="w-[37.6rem] h-[24.8]" src={logo} alt="" />
-            </div>
-            <div className="ml-28">
-              <h1 className="text-[3rem]">HOME OF MOVIES</h1>
-              <ul>
-                <li className="list-disc">WATCH MOVIES FOR FREE </li>
-                <li className="list-disc">DOWNLOAD MOVIES TO WORK OFFLINE</li>
-                <li className="list-disc">WATCH MOVIES ANYWHERE</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-[50%] w-full flex items-center flex-col justify-center">
-          <div className="">
-            <div className="text-[10rem] text-[#ff1717]">SIGNUP</div>
-            {/* form section */}
-            <Form />
-            {/* form ends here */}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-bgContainer h-screen bg-cover w-full">
+			<div className="bg-black h-screen bg-opacity-70 flex flex-col md:flex-row items-center">
+				{/* Left Side - Image and Text */}
+				<div className="lg:flex hidden items-center justify-center h-screen w-full md:w-1/2">
+					<div className="h-[80vh] w-[80%] text-white text-center">
+						<div>
+							<img className="w-[37.6rem] h-[24.8rem]" src={logo} alt="" />
+						</div>
+						<div className="mt-10">
+							<h1 className="text-3xl md:text-5xl">HOME OF MOVIES</h1>
+							<ul>
+								<li className="list-disc">WATCH MOVIES FOR FREE</li>
+								<li className="list-disc">DOWNLOAD MOVIES TO WATCH OFFLINE</li>
+								<li className="list-disc">WATCH MOVIES ANYWHERE</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				{/* Right Side - Signup Form */}
+				<div className="w-full md:w-[50%] flex items-center flex-col justify-center">
+					<div className="text-4xl md:text-6xl text-[#ff1717] text-center mb-8">
+						SIGNUP
+					</div>
+					{/* Form section */}
+					<Form />
+					{/* Form ends here */}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Container;
