8fa1c5b8327b13074f32242fa5bf7a18933867e5
Oddisy <odunayoogunleye200@gmail.com>
2023-10-04 04:37:14 +0100

    css updated

--- src/pages/signuppage/signuppage.jsx
+++ src/pages/signuppage/signuppage.jsx
@@ -2,15 +2,15 @@ import {logo} from "../../components/exports";
 import SignUpForm from "../../components/form/signup";
 function SignUpContainer() {
 	return (
-		<div className=" bg-[url('./src/assets/bgcontainer.png')] h-[1100px] bg-cover w-full">
+		<div className=" bg-[url('./src/assets/bgcontainer.png')] h-[1150px] bg-cover w-full">
 			<div className="bg-black h-screen bg-opacity-70 flex flex-col md:flex-row items-center">
 				{/* Left Side - Image and Text */}
 				<div className="lg:flex items-center justify-center h-screen w-full md:w-1/2">
-					<div className=" ml-10 flex justify-center flex-col  h-[80vh] w-[80%] text-white">
+					<div className=" ml-10 mb-16 flex justify-center flex-col h-[55vh] w-[80%] text-white">
 						<div className=" animate-pulse  transition-transform duration-[0.5s] ">
 							<img className="  w-[37.6rem] h-[24.8rem]" src={logo} alt="" />
 						</div>
-						<div className=" mt-10">
+						<div className=" mt-0 md:mt-4 lg:mt-10">
 							<h1 className="text-3xl md:text-5xl">HOME OF MOVIES</h1>
 							<ul>
 								<li className="list-disc">WATCH MOVIES FOR FREE</li>
