2b39bb533490a31d73a791e5d1fda7ab409dd75e
Oddisy <odunayoogunleye200@gmail.com>
2023-10-04 11:11:58 +0100

    updated

--- src/pages/signuppage/signuppage.jsx
+++ src/pages/signuppage/signuppage.jsx
@@ -5,7 +5,7 @@ function SignUpContainer() {
 	// bg-[url('./src/components/assets/bgcontainer.png')]
 
 	return (
-		<div className="bg-cover w-full bg-bgContainer">
+		<div className="bg-cover md:h-screen lg:h-screen w-full bg-bgContainer">
 			<div className="bg-black h-full bg-opacity-70 flex flex-col md:flex-row items-center">
 				{/* Left Side - Image and Text */}
 				<div className="lg:flex items-center justify-center  w-full md:w-1/2">
@@ -16,9 +16,11 @@ function SignUpContainer() {
 						<div className=" mt-0 md:mt-4 lg:mt-10">
 							<h1 className="text-3xl md:text-5xl">HOME OF MOVIES</h1>
 							<ul>
-								<li className="list-disc">WATCH MOVIES FOR FREE</li>
-								<li className="list-disc">DOWNLOAD MOVIES TO WATCH OFFLINE</li>
-								<li className="list-disc">WATCH MOVIES ANYWHERE</li>
+								<li className="text-sm list-disc">WATCH MOVIES FOR FREE</li>
+								<li className="text-sm list-disc">
+									DOWNLOAD MOVIES TO WATCH OFFLINE
+								</li>
+								<li className="text-sm list-disc">WATCH MOVIES ANYWHERE</li>
 							</ul>
 						</div>
 					</div>
