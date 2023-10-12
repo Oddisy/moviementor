d1726a69015eba1d8cbda53ae0247854d992c73a
Oddisy <odunayoogunleye200@gmail.com>
2023-10-12 21:53:07 +0100

    updated

--- src/pages/signuppage/signuppage.jsx
+++ src/pages/signuppage/signuppage.jsx
@@ -11,7 +11,7 @@ function SignUpContainer() {
 				<div className="lg:flex items-center justify-center  w-full md:w-1/2">
 					<div className=" ml-10 mb-16 md:mb-8 lg:mb-4 flex justify-center flex-col h-[50vh] w-[80%] text-white">
 						<div className=" animate-pulse  transition-transform duration-[0.5s] ">
-							<img className="  w-[37.6rem] h-[24.8rem]" src={logo} alt="" />
+							<img className="  w-[37.6rem] h-[15.8rem]" src={logo} alt="" />
 						</div>
 						<div className=" mt-0 md:mt-4 lg:mt-10">
 							<h1 className="text-3xl md:text-5xl">HOME OF MOVIES</h1>
@@ -27,8 +27,8 @@ function SignUpContainer() {
 				</div>
 
 				{/* Right Side - Signup Form */}
-				<div className="w-full mt-20 md:w-[50%] lg:w-[70%] flex items-center flex-col justify-center">
-					<div className="text-4xl md:text-6xl  text-[#ff1717] text-center mb-8">
+				<div className="w-full md:w-[50%] lg:w-[70%] flex items-center flex-col justify-center">
+					<div className="text-4xl md:text-6xl  text-[#ff1717] text-center mb-4">
 						SIGNUP
 					</div>
 					{/* Form section */}
