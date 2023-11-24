bc268633d3d43132c944cc4541b7f2c24d111542
Oddisy <odunayoogunleye200@gmail.com>
2023-11-24 17:44:18 +0100

    updated

--- src/pages/banner/mainbanner/mainbanner.jsx
+++ src/pages/banner/mainbanner/mainbanner.jsx
@@ -7,40 +7,29 @@ import Leftbanner from "../leftbanner/leftbanner";
 import Rightbanner from "../rightbanner/rightbanner";
 
 function Mainbanner() {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 2500, // Slide transition duration
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true, // Enable autoplay
+		autoplaySpeed: 3000, // Set autoplay speed to 3 seconds (3000ms)
+	};
 	const bannerStyle = {
 		backgroundSize: "cover",
 		backgroundPosition: "center",
 	};
 
 	return (
-		<div>
-			<div
-				className="relative flex flex-col md:flex-row   lg:min-h-[73vh] carousel"
-				style={bannerStyle}
-			>
-				<div className="bg-black w-full bg-opacity-50 absolute h-full"></div>
+		<div
+			className="relative flex flex-col md:flex-row   lg:min-h-[73vh] carousel"
+			style={bannerStyle}
+		>
+			<div className="bg-black w-full bg-opacity-50 absolute h-full"></div>
 
-				<Leftbanner className="w-full md:w-1/2" />
-				<Rightbanner className="w-full md:w-1/2" />
-			</div>
-			<div
-				className="relative flex flex-col md:flex-row   lg:min-h-[73vh] carousel"
-				style={bannerStyle}
-			>
-				<div className="bg-black w-full bg-opacity-50 absolute h-full"></div>
-
-				<Leftbanner className="w-full md:w-1/2" />
-				<Rightbanner className="w-full md:w-1/2" />
-			</div>
-			<div
-				className="relative flex flex-col md:flex-row   lg:min-h-[73vh] carousel"
-				style={bannerStyle}
-			>
-				<div className="bg-black w-full bg-opacity-50 absolute h-full"></div>
-
-				<Leftbanner className="w-full md:w-1/2" />
-				<Rightbanner className="w-full md:w-1/2" />
-			</div>
+			<Leftbanner className="w-full md:w-1/2" />
+			<Rightbanner className="w-full md:w-1/2" />
 		</div>
 	);
 }
