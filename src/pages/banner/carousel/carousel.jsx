8192519067c1df3d20ee69255745cb20fa77246f
Oddisy <odunayoogunleye200@gmail.com>
2023-11-25 18:29:07 +0100

    updated

--- src/pages/banner/carousel/carousel.jsx
+++ src/pages/banner/carousel/carousel.jsx
@@ -1,42 +1,60 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./carousel.css";
-const AutoSlideCarousel = () => {
-	const settings = {
-		dots: true,
-		infinite: true,
-		speed: 2500, // Slide transition duration
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true, // Enable autoplay
-		autoplaySpeed: 3000, // Set autoplay speed to 1 second (1000ms)
-	};
+import React, {useState, useEffect} from "react";
+import "./mainbanner.css";
+import Leftbanner from "../leftbanner/leftbanner";
+import Rightbanner from "../rightbanner/rightbanner";
+
+function Mainbanner() {
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	// Define your banners here or fetch them from an array or API
+	const banners = [
+		{
+			Left: <Leftbanner className="w-full md:w-1/2" />,
+			Right: <Rightbanner className="w-full md:w-1/2" />,
+		},
+		{
+			Left: <Leftbanner className="w-full md:w-1/2" />,
+			Right: <Rightbanner className="w-full md:w-1/2" />,
+		},
+		// Add more banner objects as needed
+	];
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			// Increment the index to display the next banner
+			setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
+		}, 5000); // Change banners every 5 seconds (adjust as needed)
+
+		return () => clearInterval(interval);
+	}, [banners.length]);
 
 	return (
-		<Slider className="h-44 flex items-center mt-20" {...settings}>
-			<div>
-				<div className="card">
-					<h3>Card 1</h3>
-					<p>Description for Card 1</p>
+		<div className="mainbanner">
+			<div className="relative flex flex-col md:flex-row lg:min-h-[73vh] carousel">
+				<div className="bg-black w-full bg-opacity-50 absolute h-full"></div>
+				<div className="banner-container">
+					<div
+						className="banner-left"
+						style={{
+							opacity: currentIndex === 0 ? 1 : 0,
+							transition: "opacity 1s ease-in-out",
+						}}
+					>
+						{banners[currentIndex].Left}
+					</div>
+					<div
+						className="banner-right"
+						style={{
+							opacity: currentIndex === 0 ? 1 : 0,
+							transition: "opacity 1s ease-in-out",
+						}}
+					>
+						{banners[currentIndex].Right}
+					</div>
 				</div>
 			</div>
-			<div>
-				<div className="card">
-					<h3>Card 2</h3>
-					<p>Description for Card 2</p>
-				</div>
-			</div>
-			<div>
-				<div className="card">
-					<h3>Card 3</h3>
-					<p>Description for Card 3</p>
-				</div>
-			</div>
-			{/* Add more slides as needed */}
-		</Slider>
+		</div>
 	);
-};
+}
 
-export default AutoSlideCarousel;
+export default Mainbanner;
--- src/pages/banner/mainbanner/mainbanner.jsx
+++ src/pages/banner/mainbanner/mainbanner.jsx
@@ -1,35 +1,25 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "./mainbanner.css";
 import Leftbanner from "../leftbanner/leftbanner";
 import Rightbanner from "../rightbanner/rightbanner";
 
 function Mainbanner() {
-	const settings = {
-		dots: true,
-		infinite: true,
-		speed: 2500, // Slide transition duration
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true, // Enable autoplay
-		autoplaySpeed: 3000, // Set autoplay speed to 3 seconds (3000ms)
-	};
 	const bannerStyle = {
 		backgroundSize: "cover",
 		backgroundPosition: "center",
 	};
 
 	return (
-		<div
-			className="relative flex flex-col md:flex-row   lg:min-h-[73vh] carousel"
-			style={bannerStyle}
-		>
-			<div className="bg-black w-full bg-opacity-50 absolute h-full"></div>
+		<div className="mainbanner">
+			<div
+				className="relative flex flex-col md:flex-row   lg:min-h-[73vh] carousel"
+				style={bannerStyle}
+			>
+				<div className="bg-black w-full bg-opacity-50 absolute h-full"></div>
 
-			<Leftbanner className="w-full md:w-1/2" />
-			<Rightbanner className="w-full md:w-1/2" />
+				<Leftbanner className="w-full md:w-1/2" />
+				<Rightbanner className="w-full md:w-1/2" />
+			</div>
 		</div>
 	);
 }
